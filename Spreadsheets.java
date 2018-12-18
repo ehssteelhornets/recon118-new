d4cd19671c188a595b32ec0d5e46e83f895e7335
Steel Hornets Robotics <ehssteelhornets@gmail.com>
2018-12-18 15:21:41 -0500

    12/18/18 3:21
    First actual commit

--- Spreadsheets.java
+++ Spreadsheets.java
@@ -3,7 +3,7 @@ package recon118;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-//Add libraries BIGLY
+//Add libraries
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
