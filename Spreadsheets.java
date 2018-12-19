acbe6cbc07dc6bf452d1d59902225c2839b159c9
dtleight <43557951+dtleight@users.noreply.github.com>
2018-12-19 16:11:50 -0500

    Changes 4:11
    Added in backend for Ranking system off of Comparators.

--- Spreadsheets.java
+++ Spreadsheets.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 //Add libraries
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -40,28 +41,28 @@ class Spreadsheets {
     private static final String CREDENTIALS_FILE_PATH = "credentials.json";
     private static final String spreadsheetID = "1peZKQGBokN1ILdY9Hqhx8x9HzCtHHczAfVjfkdjVLjU";
     private static final String[] spreadsheetName = {"ContactInfo", "MainSchedule", "Sample"};
-    
+
     private static Sheets service = null;
-    
+
     public static void main() {
         initialize();
         List<List<Object>> cells = readCells("Sheet1");
         /*
         for (List<Object> a: cells) {
-            for (Object b: a) {
-                System.out.print(b);
-                System.out.print("\t");
-            }
-            System.out.println();
+        for (Object b: a) {
+        System.out.print(b);
+        System.out.print("\t");
+        }
+        System.out.println();
         }*/
         LinkedList<Team> teams = new LinkedList<Team>();
         for (List<Object> row: cells) {
             if (teamContains(teams,(int)row.get(2))==-1) 
                 teams.add(new Team("",(int)row.get(2)));
-            
+
         }
     }
-    
+
     private static int teamContains(LinkedList<Team> teams, int teamNum) {
         for (int i = 0; i < teams.size(); i++) {
             if (teams.get(i).getNum()==teamNum) {
@@ -70,23 +71,23 @@ class Spreadsheets {
         }
         return -1;
     }
-    
+
     public static void initialize() {
         //Build a new authorized API service
         try {
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
             service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+            .setApplicationName(APPLICATION_NAME)
+            .build();
         } catch (Exception e) {throw new RuntimeException(e);}
     }
 
     /**
-    * Creates an authorized Credential object.
-    * @param HTTP_TRANSPORT The network HTTP Transport.
-    * @return An authorized Credential object.
-    * @throws IOException If the credentials.json file cannot be found.
-    */
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         InputStream in = Spreadsheets.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -97,7 +98,7 @@ class Spreadsheets {
         SCOPES.add(SheetsScopes.SPREADSHEETS);
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
             .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
             .setAccessType("offline")
             .build();
@@ -105,15 +106,15 @@ class Spreadsheets {
     }
 
     /**
-    * Prints the names and majors of students in a sample spreadsheet:
-    */
+     * Prints the names and majors of students in a sample spreadsheet:
+     */
     public static List<List<Object>> readCells(final String range){
         try {
             ValueRange response = service.spreadsheets().values()
                 .get(spreadsheetID, range)
                 .execute();
             List<List<Object>> values = response.getValues();
-            
+
             System.out.println("Read range |"+range+"| of " + spreadsheetID + " successfully");
             if (values == null || values.isEmpty()) {
                 System.out.println("No data found.");
@@ -122,19 +123,19 @@ class Spreadsheets {
             return values;
         } catch (Exception e) {throw new RuntimeException(e);}
     }
-    
+
     /**
-    * Updates Cells of range with values
-    */
+     * Updates Cells of range with values
+     */
     public static void writeCells(final String range, final List<List<Object>> values) {
         try {
             ValueRange body = new ValueRange()
                 .setValues(values);
-            
+
             service.spreadsheets().values().update(spreadsheetID, range, body)
-                .setValueInputOption("RAW")
-                .execute();
-            
+            .setValueInputOption("RAW")
+            .execute();
+
             //System.out.println("Updated Cells of " +  + "/" + range.substring(0,range.indexOf("!")) + ":");
             char startCellLetter = range.charAt(range.indexOf("!")+1);
             String startCellNum = range.substring(range.indexOf("!")+2,range.indexOf(":"));
@@ -164,30 +165,46 @@ class Spreadsheets {
             this.name = name;
             this.teamNumber = teamNumber;
         }
-    
+
         public void addScore(int gameScore, int penaltys)
         {
             totalScore += gameScore;
             penaltylessScore += gameScore - penaltys;
         }
-    
+
         public String toString()
         {
             return "Team " + teamNumber + "," + name;
         }
-    
+
         public void addWin()
         {
             wins++;
         }
-    
+
         public void addLoss()
         {
             losses++;
         }
-        
+
         public int getNum() {
             return teamNumber;
+        }
+    }
+    private class Rankings
+    {
+        private Comparator sorter = null;
+        private LinkedList teams = null;
+        public Rankings(String sortType, LinkedList teams)
+        {
+            this.teams = teams;
+            //Fill in a comparator
+            this.sorter = null;
+        }
+
+        public void sort(String sortType)
+        {
+            Collections.sort(teams,sorter);
         }
     }
 }
