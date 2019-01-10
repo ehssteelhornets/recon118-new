6de2a6ff5362b2396624ba84b299a5edc3b9001d
dtleight <43557951+dtleight@users.noreply.github.com>
2019-01-10 11:56:14 -0500

    Changes for school BlueJ

--- Game.java
+++ Game.java
@@ -1,13 +1,20 @@
-package recon118;
+
 import java.util.Set;
 import java.util.TreeSet;
 //A game is a collection of 4 matches with the same game numbers
 public class Game
 {
     private int gameNum;
+    private int blueScore;
+    private int redScore;
     private Set<Match> matches = new TreeSet<Match>();
     public Game(int gameNum)
     {
-        
+
+    }
+
+    public int getScore()
+    {
+        return 0;
     }
 }
